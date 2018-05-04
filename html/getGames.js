ff31f68649fc54f4fb47303101df1bef8b95a1b8
GreenMobius <tchatfield16@gmail.com>
2018-05-03 21:58:55 -0400

    homepage successfully shows list of games that updates on reload of page or search button.

--- html/getGames.js
+++ html/getGames.js
@@ -2,25 +2,28 @@ const express = require('express');
 const app = express();
 const sql = require('mssql');
 
-const config = (
-	user: BoardGames38,
+const config = {
+	user: 'BoardGames38',
 	password: 'TwilightImperium20',
 	server: 'golem.csse.rose-hulman.edu',
-	database: 'BoardGamesTracker',,
-)
+	database: 'BoardGameTracker',
+};
 
-
-try {
-	sql.close();
-	const pool = await sql.connect(config);
-	const result = await pool.request()
-		.execute('Execute GetGames');
-	let toShow = [];
-	for (let x in result.recordset) {
-		toShow.push(result.recordset[x]);
+async function getGames() {
+	console.log("Requesting Games From Server");
+	try {
+		sql.close();
+		const pool = await sql.connect(config);
+		const result = await pool.request().query('EXECUTE GetGames');
+		let toShow = [];
+		for(let x in result.recordset) {
+			toShow.push(result.recordset[x]);
+		}
+		sql.close();
+		console.log("Received Games From Server");
+		return(toShow);
+	} catch (err){
+		console.log(err);
+		sql.close();
 	}
-	alert(toShow);
-} catch (err) {
-	alert(err);
-	sql.close();
 }
--- nodeBGames/server.js
+++ nodeBGames/server.js
@@ -11,36 +11,35 @@ const config = {
  
 app.use(express.static(__dirname + '/public')); // exposes index.html, per below
 
-app.get('/request', function(req, res){
-	console.log("requesting");
+app.get('/request', function(req, res) {
+	console.log("Beginning Server Request");
 	new Promise(
 		function (resolve, reject){
 			resolve(getGames());
 		}
 	).then(
 		function (fulfilled) {
-			console.log(JSON.stringify(fulfilled));
+			//console.log(JSON.stringify(fulfilled));
 			//res.json('[{"name":"game1","description":"test 1","complexity":3,"playTime":"10 hours","numPlayers":5},{"name":"game2","description":"test 2","complexity":2,"playTime":"3 hours","numPlayers":2},{"name":"game3","description":"test 3","complexity":3,"playTime":"3 hours","numPlayers":3}]');
-			res.send(fulfilled);
-			console.log("requesting done");
+			res.json(fulfilled);
+			console.log("Completed Server Request");
 		}
 	);
 	
 });
 
 async function getGames() {
-	console.log("getgame");
+	console.log("Requesting Games From Server");
 	try {
 		sql.close();
 		const pool = await sql.connect(config);
 		const result = await pool.request().query('EXECUTE GetGames');
-		//console.log(result.recordset);
 		let toShow = [];
 		for(let x in result.recordset) {
-			console.log(result.recordset[x]);
 			toShow.push(result.recordset[x]);
 		}
-		sql.close()
+		sql.close();
+		console.log("Received Games From Server");
 		return(toShow);
 	} catch (err){
 		console.log(err);
@@ -49,5 +48,5 @@ async function getGames() {
 }
 
 var server = app.listen(80, function () {
- console.log('Server is running.. on Port 80');
+ console.log('Server Running on Port 80');
 });
