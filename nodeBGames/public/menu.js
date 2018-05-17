4231c19922634efc27ca4daa40263ffe24bf5079
GreenMobius <tchatfield16@gmail.com>
2018-05-16 20:29:08 -0400

    added menu script to keep everything similar

---
+++ nodeBGames/public/menu.js
@@ -0,0 +1,29 @@
+function goToHomepage() {
+    window.setTimeout(function() {
+                        window.location.replace('/home');
+    },100);
+}
+
+function goToClubpage() {
+    window.setTimeout(function() {
+                        window.location.replace('/club');
+    },100);
+}
+
+function goToProfile() {
+    window.setTimeout(function() {
+                        window.location.replace('/profile');
+    },100);
+}
+
+function goToSuggestion() {
+    window.setTimeout(function() {
+                        window.location.replace('/suggest');
+    },100);
+}
+
+function goToHelp() {
+    window.setTimeout(function() {
+                        window.location.replace('/help');
+    }, 100);
+}
